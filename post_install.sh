7b1ad87ebab62f2d8801bec9f5581d274de6daaa
Krzysztof Sinica <krzysztof.sinica@gmail.com>
2020-10-06 18:14:37 +0200

    WIP

--- post_install.sh
+++ post_install.sh
@@ -5,13 +5,11 @@ curl -fsSL https://code-server.dev/install.sh | sh -s -- --version 3.5.0
 sysrc -f /etc/rc.conf code_server_enable="YES"
 sysrc -f /etc/rc.conf nginx_enable="YES"
 
-pw user add -n code -d /home/code -m
-mkdir -p /home/code/.config/code-server/
-echo "bind-addr: 127.0.0.1:8080" > /home/code/.config/code-server/config.yaml
-echo "auth: password" >> /home/code/.config/code-server/config.yaml
-echo "password: changeme" >> /home/code/.config/code-server/config.yaml
-echo "cert: false" >> /home/code/.config/code-server/config.yaml
-chown -R code /home/code/.config
+mkdir -p /root/.config/code-server/
+echo "bind-addr: 127.0.0.1:8080" > /root/.config/code-server/config.yaml
+echo "auth: password" >> /root/.config/code-server/config.yaml
+echo "password: changeme" >> /root/.config/code-server/config.yaml
+echo "cert: false" >> /root/.config/code-server/config.yaml
 
 service code_server start
 service nginx start
