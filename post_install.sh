1a645e1e1d5303bc033d1620e625ce0080395b76
Krzysztof Sinica <krzysztof.sinica@gmail.com>
2020-10-06 15:19:21 +0200

    WIP

--- post_install.sh
+++ post_install.sh
@@ -2,9 +2,8 @@
 
 curl -fsSL https://code-server.dev/install.sh | sh -s -- --version 3.5.0
 
-sysrc -f /etc/rc.conf code_server_enable=YES
-sysrc -f /etc/rc.conf nginx_enable=YES
+sysrc -f /etc/rc.conf code_server_enable="YES"
+sysrc -f /etc/rc.conf nginx_enable="YES"
 
-echo "Starting nginx..."
 service code_server start
 service nginx start
