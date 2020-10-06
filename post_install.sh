5da7a497e532a333e4ade43915bd33b1ef2413bf
Krzysztof Sinica <krzysztof.sinica@gmail.com>
2020-10-06 21:01:34 +0200

    WIP

--- post_install.sh
+++ post_install.sh
@@ -5,6 +5,17 @@ curl -fsSL https://code-server.dev/install.sh | sh -s -- --version 3.5.0
 sysrc -f /etc/rc.conf code_server_enable="YES"
 sysrc -f /etc/rc.conf nginx_enable="YES"
 
+# based on https://nickolaskraus.org/articles/how-to-create-a-self-signed-certificate-for-nginx-on-macos/
+mkdir -p /usr/local/etc/ssl/private
+mkdir -p /usr/local/etc/ssl/certs
+openssl req \
+  -x509 -nodes -days 365 -newkey rsa:2048 \
+  -subj "/CN=code-server" \
+  -keyout /usr/local/etc/ssl/private/self-signed.key \
+  -out /usr/local/etc/ssl/certs/self-signed.crt
+openssl dhparam -out /usr/local/etc/ssl/certs/dhparam.pem 128
+
+
 pw user add -n code -m
 
 service code_server start
