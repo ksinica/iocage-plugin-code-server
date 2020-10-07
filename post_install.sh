cff20a49c44725e8d60e44244c7a582c3fb45fa0
Krzysztof Sinica <krzysztof.sinica@gmail.com>
2020-10-07 13:07:41 +0200

    WIP

--- post_install.sh
+++ post_install.sh
@@ -11,12 +11,16 @@ mkdir -p /usr/local/etc/ssl/certs
 openssl req \
   -x509 -nodes -days 365 -newkey rsa:2048 \
   -subj "/CN=code-server" \
-  -keyout /usr/local/etc/ssl/private/self-signed.key \
-  -out /usr/local/etc/ssl/certs/self-signed.crt
-openssl dhparam -out /usr/local/etc/ssl/certs/dhparam.pem 128
+  -keyout /usr/local/etc/ssl/private/cakey.pem \
+  -out /usr/local/etc/ssl/certs/cacert.crt
+openssl dhparam -out /usr/local/etc/ssl/certs/dhparam.pem 2048
 
-
-pw user add -n code -m
+pw user add -n coder -m
 
 service code_server start
 service nginx start
+
+sleep 5
+
+echo "Password: $(pluginget password)" > /root/PLUGIN_INFO
+echo "Certificate: $(openssl x509 -in /usr/local/etc/ssl/certs/cacert.crt -noout -text)" >> /root/PLUGIN_INFO
